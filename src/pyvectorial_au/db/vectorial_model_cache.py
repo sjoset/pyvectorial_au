fe6800ab72d22bbbd0dbb88308d386caa0692c27
Shawn O <szo0032@auburn.edu>
2024-04-03 10:24:11 -0500

    begin reworking database code

--- src/pyvectorial_au/db/vectorial_model_cache.py
+++ src/pyvectorial_au/db/vectorial_model_cache.py
@@ -16,6 +16,15 @@ vm_cache_engine: Optional[Engine] = None
 vm_cache_session = None
 
 
+"""
+From the sqlalchemy docs:
+When using an Engine with multiple Python processes,
+such as when using os.fork or Python multiprocessing,
+it’s important that the engine is initialized per process.
+See Using Connection Pools with Multiprocessing or os.fork() for details.
+"""
+
+
 class VMCachedBase(DeclarativeBase):
     pass
 
@@ -29,13 +38,17 @@ class VMCached(VMCachedBase):
     vectorial_model_version: Mapped[str] = mapped_column(nullable=False)
 
 
-def initialize_vectorial_model_cache(vectorial_model_cache_dir: pathlib.Path) -> None:
+def initialize_vectorial_model_cache(
+    vectorial_model_cache_dir: pathlib.Path,
+    vectorial_model_cache_filename: pathlib.Path = pathlib.Path("vmcache.sqlite3"),
+) -> None:
     global vm_cache_engine, vm_cache_session
 
     if vm_cache_engine is not None or vm_cache_session is not None:
         return
 
-    full_db_path = vectorial_model_cache_dir / pathlib.Path("vmcache.sqlite3")
+    # full_db_path = vectorial_model_cache_dir / pathlib.Path("vmcache.sqlite3")
+    full_db_path = vectorial_model_cache_dir.joinpath(vectorial_model_cache_filename)
     vm_cache_engine = create_engine(f"sqlite:///{full_db_path}", poolclass=NullPool)
 
     VMCachedBase.metadata.create_all(bind=vm_cache_engine)
